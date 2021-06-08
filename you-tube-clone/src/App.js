b456fa1b4b8d6b7211ac11c0727b00d6b15d73dd
karunesc <karunesh.chhabra@nokia.com>
2021-06-08 10:19:09 +0530

    you tube view without search bar

--- you-tube-clone/src/App.js
+++ you-tube-clone/src/App.js
@@ -1,22 +1,31 @@
-import './App.css';
+import "./App.css";
 
 import Header from "./Header";
-import RecommendedVideos from './RecommendedVideos';
-import SideView from './SideView';
-
-
+import RecommendedVideos from "./RecommendedVideos";
+import SideView from "./SideView";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="app">
-      <Header></Header>
-      <div className="bottom__panel">
-    <SideView/>
-    <RecommendedVideos/>
-    </div>
-
-
-     
+      <Router>
+        <Switch>
+          <Route path="/search/:searchText" exact>
+            <Header></Header>
+            <h1>Search Page</h1>
+            <div className="bottom__panel">
+              <SideView />
+            </div>
+          </Route>
+          <Route path="/">
+            <Header></Header>
+            <div className="bottom__panel">
+              <SideView />
+              <RecommendedVideos />
+            </div>
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
--- you-tube-clone/src/Header.js
+++ you-tube-clone/src/Header.js
@@ -1,35 +1,43 @@
-
-
-import React from 'react'
-import VideoCallIcon from '@material-ui/icons/VideoCall';
-import NotificationsIcon from '@material-ui/icons/Notifications';
-import MenuIcon from '@material-ui/icons/Menu';
-import SearchIcon from '@material-ui/icons/Search';
-import "./Header.css"
+import React, { useState } from "react";
+import VideoCallIcon from "@material-ui/icons/VideoCall";
+import NotificationsIcon from "@material-ui/icons/Notifications";
+import MenuIcon from "@material-ui/icons/Menu";
+import SearchIcon from "@material-ui/icons/Search";
+import { Link } from "react-router-dom";
+import "./Header.css";
 
 function Header() {
-    return (
-        <div className="header">
-            <div className="header__left">
-                <MenuIcon/>
-                <img className="image_icon" alt="youTube logo"
-                src="https://upload.wikimedia.org/wikipedia/commons/b/b8/YouTube_Logo_2017.svg"
-                />
-            </div>
-            <div className="header__input">
+  const [inputField, setInputField] = useState("");
 
-  <input placeholder="Search...."/>
-     <SearchIcon className="header__input__button"/>
-  </div>
-
-  <div className="header__right">
-      
-      <VideoCallIcon className="header__icon"/>
-      <NotificationsIcon className="header__icon"/>
+  return (
+    <div className="header">
+      <div className="header__left">
+        <MenuIcon />
+        <Link to="/">
+          <img
+            className="image_icon"
+            alt="youTube logo"
+            src="https://upload.wikimedia.org/wikipedia/commons/b/b8/YouTube_Logo_2017.svg"
+          />
+        </Link>
       </div>
-            
-        </div>
-    )
+      <div className="header__input">
+        <input
+          placeholder="Search...."
+          value={inputField}
+          onChange={(e) => setInputField(e.target.value)}
+        />
+        <Link to={`/search/${inputField}`}>
+          <SearchIcon className="header__input__button" />
+        </Link>
+      </div>
+
+      <div className="header__right">
+        <VideoCallIcon className="header__icon" />
+        <NotificationsIcon className="header__icon" />
+      </div>
+    </div>
+  );
 }
 
-export default Header
+export default Header;
